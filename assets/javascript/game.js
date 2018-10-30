ea854e2825bcec2ca97780757bcd5532db68bdde
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-10-29 17:41:01 -0700

    got the counters working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,28 @@
 alert('Press any letter on the keyboard to begin playing');
 
+var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 var won = 0;
 var lost = 0;
-var attempts = 9;
-var guessedSoFar = [];
+var attempts = 10;
+var guessed = [];
 
-function randLetter() {
+var letter = alpha[Math.floor(Math.random() * alpha.length)];
 
-    var letters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-    
-    var letter = letters[Math.floor(Math.random() * letters.length)];
-    
-    for(var i = 0; i < letters.length; i++) {
-        if(i == letter) {
-            alert("You guessed the letter I was thinking of!");
-            won++;
-        } else {
-            alert("You failed to guess the correct letter. Try again!");
-            lost++;
-        }
+document.onkeypress = function(event) {
+    var guess = event.key;
+
+    if(guess === letter) {
+        won++;
+    } else {
+        attempts--;
     }
+
+    if(attempts === 0) {
+        lost++;
+    }
+
+    document.getElementById('wins').innerHTML = "Wins: " + won;
+    document.getElementById('losses').innerHTML = "Losses: " + lost;
+    document.getElementById('guesses-left').innerHTML = "Guesses Left: " + attempts;
 };
