2738cf4bfdf334f26407db73d14762cfdb96948f
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-10-29 18:14:09 -0700

    guessed so far appended

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,27 +2,31 @@ alert('Press any letter on the keyboard to begin playing');
 
 var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var won = 0;
-var lost = 0;
-var attempts = 10;
-var guessed = [];
+var wins = 0;
+var losses = 0;
+var guesses = 10;
 
 var letter = alpha[Math.floor(Math.random() * alpha.length)];
 
 document.onkeypress = function(event) {
     var guess = event.key;
-
-    if(guess === letter) {
-        won++;
+    
+    if (guess === letter) {
+        wins++;
     } else {
-        attempts--;
+        guesses--;
+    }
+    
+    if (guess === letter) {
+        guesses = 10;
     }
 
-    if(attempts === 0) {
-        lost++;
+    if (guesses === 0) {
+        losses++;
     }
-
-    document.getElementById('wins').innerHTML = "Wins: " + won;
-    document.getElementById('losses').innerHTML = "Losses: " + lost;
-    document.getElementById('guesses-left').innerHTML = "Guesses Left: " + attempts;
+    
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "Losses: " + losses;
+    document.getElementById('guesses-left').innerHTML = "Guesses Left: " + guesses;
+    document.getElementById('guessed').innerHTML = "Your Guesses so Far: " + guess;
 };
