a32ac50da9eb8f65b4064ad3139b10220d34d3b6
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-11-11 20:56:10 -0700

    updated game.js to reset counter when losses++

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,17 @@
 alert('Press any letter on the keyboard to begin playing');
 
-var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 
-var letter = alpha[Math.floor(Math.random() * alpha.length)];
+var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 document.onkeypress = function(event) {
     var guess = event.key;
+    
+    console.log(event.key);
     
     if (guess === letter) {
         wins++;
@@ -22,11 +24,21 @@ document.onkeypress = function(event) {
     }
 
     if (guesses === 0) {
-        losses++;
+        losses++,
+        guesses = 10;
     }
-    
+
     document.getElementById('wins').innerHTML = "Wins: " + wins;
     document.getElementById('losses').innerHTML = "Losses: " + losses;
     document.getElementById('guesses-left').innerHTML = "Guesses Left: " + guesses;
     document.getElementById('guessed').innerHTML = "Your Guesses so Far: " + guess;
 };
+
+
+// So you can just put line 9 into the if statement to reset your letter.
+// Or what you can do is put the variables inside a function, and just call it a reset function or something
+// and put that function into the if statement, depending on what happen
+
+// So you can just put line 9 into the if statement to reset your letter.
+// Or what you can do is put the variables inside a function, and just call it a reset function or something
+// and put that function into the if statement, depending on what happen
